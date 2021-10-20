dc952fc65fe2272acff0a1f8f7f7b345bb097ab9
Farah Sheherin <farahsheherin@gmail.com>
2021-10-20 11:23:00 -0400

    adding build folder

---
+++ build/precache-manifest.176135c102f75e52f6c445e8446fa18a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a416616939d346796fd6",
+    "url": "/farahsheherin/COMP3104/static/css/main.c52f6eb1.chunk.css"
+  },
+  {
+    "revision": "a416616939d346796fd6",
+    "url": "/farahsheherin/COMP3104/static/js/main.a4166169.chunk.js"
+  },
+  {
+    "revision": "99734f3ad7ec5c0a698c",
+    "url": "/farahsheherin/COMP3104/static/js/runtime~main.99734f3a.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/farahsheherin/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "3630199f242ab6579b6ceb7122b68c37",
+    "url": "/farahsheherin/COMP3104/index.html"
+  }
+];
