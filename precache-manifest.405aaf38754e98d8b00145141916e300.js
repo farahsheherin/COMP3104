5464d40107931a65d876c0e28b6a334817db7c0e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-21 02:14:50 +0000

    Deploy farahsheherin/COMP3104 to github.com/farahsheherin/COMP3104.git:gh-pages

---
+++ precache-manifest.405aaf38754e98d8b00145141916e300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c10c10fce887c32919c5",
+    "url": "/farahsheherin/COMP3104/static/css/main.c8234812.chunk.css"
+  },
+  {
+    "revision": "c10c10fce887c32919c5",
+    "url": "/farahsheherin/COMP3104/static/js/main.c10c10fc.chunk.js"
+  },
+  {
+    "revision": "99734f3ad7ec5c0a698c",
+    "url": "/farahsheherin/COMP3104/static/js/runtime~main.99734f3a.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/farahsheherin/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "3c7b8dd0056d6f2bd3304d5466648619",
+    "url": "/farahsheherin/COMP3104/index.html"
+  }
+];
